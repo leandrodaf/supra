69b3396970531a494fea1aa51fd9580149dfdcec
Leandro Ferreira <leandro.daf4@gmail.com>
2017-11-08 16:15:23 -0200

    Fixed correções

--- routes/web.php
+++ routes/web.php
@@ -25,20 +25,17 @@ Route::resource('pessoas', 'PessoaController');
 Route::get(
     'alunos/getAjax',
     'PessoaController@dataAjax'
-    )
-    ->name('alunos.getAjaxSelect2');
+    )->name('alunos.getAjaxSelect2');
 
 Route::post(
     'alunos/responsaveis/{responsavel}',
     'AlunosController@updateResponsaveis'
-    )
-    ->name('alunos.updateResponsaveis');
+    )->name('alunos.updateResponsaveis');
 
 Route::post(
     'alunos/responsaveis/{responsavel}/desvincular',
     'AlunosController@desvincularAluno'
-    )
-    ->name('alunos.desvincularAluno');
+    )->name('alunos.desvincularAluno');
 
 Route::resource('alunos', 'AlunosController');
 
