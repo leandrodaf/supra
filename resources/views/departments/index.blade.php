fe130a365c46a1e4496f65ed15da14a3734c71f2
Leandro Ferreira <leandro.daf4@gmail.com>
2018-01-12 16:28:57 -0200

    fix Texto na tela de Setores

--- resources/views/departments/index.blade.php
+++ resources/views/departments/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <section class="content-header">
-        <h1 class="pull-left">departments</h1>
+        <h1 class="pull-left">Setor</h1>
         <h1 class="pull-right">
            <a class="btn btn-primary pull-right" style="margin-top: -10px;margin-bottom: 5px" href="{!! route('departments.create') !!}">Novo Setor</a>
         </h1>
