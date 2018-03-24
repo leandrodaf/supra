8885c5467021734fb24f5b4b02ed4718eeae58ea
Leandro Ferreira <leandro.daf4@gmail.com>
2018-03-24 15:34:39 -0300

    Fixeds

--- resources/views/yearClass/call.blade.php
+++ resources/views/yearClass/call.blade.php
@@ -5,7 +5,7 @@
     <div class="col-lg-1">
     </div>
     <div class="col-lg-7" style="padding-top: 10%;">
-        <button type="button" class="btn btn-block btn-bitbucket btn-lg" data-toggle="modal" data-target="#makeCall" data-loading-text="<i class='fa fa-spinner fa-spin '></i> Carregando..." id="makeCallButton" disabled="disabled">Realizar chamada</button>
-        <button type="button" class="btn btn-block btn-bitbucket btn-lg" data-toggle="modal" data-target="#alterCall" data-loading-text="<i class='fa fa-spinner fa-spin '></i> Carregando..." id="alteredCallButton" disabled="disabled">Alterar chamada</button>
+        <button type="button" class="btn btn-block btn-bitbucket btn-lg" data-toggle="modal" data-target="#makeCall" id="makeCallButton" disabled="disabled">Realizar chamada</button>
+        <button type="button" class="btn btn-block btn-bitbucket btn-lg" data-toggle="modal" data-target="#alterCall" id="alteredCallButton"disabled="disabled">Alterar chamada</button>
     </div>
 </div>
