9e6e984defe05db32ad529fbf13d8a4981308f7a
Leandro Ferreira <leandro.daf4@gmail.com>
2018-04-01 00:35:45 -0300

    Ajustes nos imports de scripts

--- resources/assets/js/custom_js/yearClass.js
+++ resources/assets/js/custom_js/yearClass.js
@@ -467,35 +467,4 @@ $(document).ready(function () {
 
     };
 
-
-//    Notification feature
-
-    $('#notifictionMessage').trumbowyg({
-        btns: [
-            ['viewHTML'],
-            ['undo', 'redo'], // Only supported in Blink browsers
-            ['strong', 'em', 'del'],
-            ['superscript', 'subscript'],
-            ['link'],
-            ['horizontalRule'],
-            ['removeformat'],
-        ],
-        lang: 'pt_br',
-        removeformatPasted: true,
-        autogrow: true
-    });
-
-    $("#exibitionDate").datepicker({
-        viewMode: "months",
-        autoclose: true,
-        daysOfWeekDisabled: [0, 6],
-        todayHighlight: true,
-        language: 'pt-BR'
-    });
-
-    if ($('meta[name="notification-title"]').attr('content').length > 0) {
-        $('#notificationModal').modal().show();
-    }
-
-
 });
