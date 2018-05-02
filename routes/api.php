9bcb2c08060cdcfbfbaeb9e0ca954c2ead75510e
Leandro Ferreira <leandro.daf4@gmail.com>
2018-05-02 15:55:57 -0300

    Ajuste nas rotas da API

--- routes/api.php
+++ routes/api.php
@@ -9,25 +9,25 @@ Route::middleware(['auth:api'])->group(function () {
         return response()->json($aluno, 200);
     });
 
-    Route::get('/alunos/atividade', function (Request $request) {
+    Route::get('/aluno/atividades', function (Request $request) {
         $aluno = \App\Models\Alunos::find($request->user()->alunos_id);
         $activities = $aluno->getActivitiesByAluno($aluno);
         return response()->json($activities, 200);
     });
 
-    Route::get('/alunos/turma', function (Request $request) {
+    Route::get('/aluno/turmas', function (Request $request) {
         $aluno = \App\Models\Alunos::find($request->user()->alunos_id);
         $turmas = $aluno->getTurmaByIdAluno($aluno);
         return response()->json($turmas, 200);
     });
 
-    Route::get('/alunos/mensagem', function (Request $request) {
+    Route::get('/aluno/mensagens', function (Request $request) {
         $aluno = \App\Models\Alunos::find($request->user()->alunos_id);
         $notifications = $aluno->notification()->whereBetween('created_at', [\Carbon\Carbon::today()->subDays(15), \Carbon\Carbon::today()->addDays(20)])->orderBy('created_at', 'asc')->get();
         return response()->json($notifications, 200);
     });
 
-    Route::get('/alunos/diario', function (Request $request) {
+    Route::get('/aluno/diarios', function (Request $request) {
         $aluno = \App\Models\Alunos::find($request->user()->alunos_id);
         $diarios = $aluno->diairy()->take(5)->get();
         return response()->json($diarios, 200);
