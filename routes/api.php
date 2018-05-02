0c38d515f75462614af1f0a432f5145d03b36151
Leandro Ferreira <leandro.daf4@gmail.com>
2018-05-02 15:06:36 -0300

    Ajustes

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,10 @@ use Illuminate\Http\Request;
 
 Route::middleware(['auth:api'])->group(function () {
 
-    Route::get('/aluno', 'API\AlunosController@aluno');
-    Route::get('/aluno/atividade', 'API\AlunosController@atividade');
-    Route::get('/aluno/turma', 'API\AlunosController@turma');
-    Route::get('/aluno/mensagem', 'API\AlunosController@mensagem');
-    Route::get('/aluno/diario', 'API\AlunosController@diario');
+    Route::get('/aluno', 'AlunosController@aluno');
+    Route::get('/aluno/atividade', 'AlunosController@atividade');
+    Route::get('/aluno/turma', 'AlunosController@turma');
+    Route::get('/aluno/mensagem', 'AlunosController@mensagem');
+    Route::get('/aluno/diario', 'AlunosController@diario');
 
 });
